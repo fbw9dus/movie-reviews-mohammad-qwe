ecd6bdd3f5fd8de8460f8dc592f6d089774d22de
mohammad saied taleb <mohammadsolingen@gmail.com>
2019-09-17 16:16:16 +0200

    mein

--- movie.js
+++ movie.js
@@ -1,3 +1,19 @@
+function show1(){
+    document.getElementById("mainView").innerHTML="Downton Abby (2019): This fall, the worldwide phenomenon Downton abbey, becomes agrand motion picture event,as the beloved Craweleys and their interpid staff prepare for the most important moment."
+}
+function show2(){
+    document.getElementById("mainView").innerHTML="Hustlers (2019): Led by career-best performance from Jennifer Lopez,Hustlers is a uniquely empowering heist drama with depth and intelligence to match its striking visual appeal."
+}
+function show3(){
+    document.getElementById("mainView").innerHTML="For Sama (2019): As intimate as it is heartbreakingly resonant, For Sama powerfully distills the difficult choices faced by citizens of war-torn regions."
+}
+function show4(){
+    document.getElementById("mainView").innerHTML="Ad Astra (2019): Ad Astra takes a visually thrilling journey through the vast reaches of space while charting an ambitios course for the heart ofthe bond between parent and child."
+}
+document.getElementsByClassName("sideView5")[0].addEventListener("click", show5)
+function show5(){
+    document.getElementById("mainView").innerHTML="Loro (2019): Loro uses the larger-than-life exploits of an infamous puplic figure to present a messily comelling snapshot of how power attracts and corrupts"
+}
 function show(){
-    document.getElementById("mainView").innerHTML=document.getElementsByClassName("star-five").value
+    document.getElementById("mainView").innerHTML=""
 }
