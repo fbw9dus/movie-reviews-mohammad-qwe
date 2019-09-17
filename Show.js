cdfe5723653a36030e940b7274d2c800e92b8f83
mohammad saied taleb <mohammadsolingen@gmail.com>
2019-09-17 12:00:08 +0200

    mein

---
+++ Show.js
@@ -0,0 +1,3 @@
+function Show() {
+    document.getElementById("mainView").innerHTML = document.getElementsByClassName("star-five").innerHTML;
+}
---
+++ movie.js
@@ -0,0 +1,3 @@
+function show(){
+    document.getElementById("mainView").innerHTML=document.getElementsByClassName("star-five").value
+}
